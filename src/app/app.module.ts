d9966d0fb804490e3439c527959632334f787967
Freddy Antayhua Saavedra <freddy32@outlook.com.pe>
2018-02-27 22:07:00 -0500

    segundo commit programa angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule } from "@angular/material";
+import { MatButtonModule, MatToolbarModule,MatIconModule } from "@angular/material";
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -21,7 +21,8 @@ import { PathNotFoundComponent } from './path-not-found/path-not-found.component
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
